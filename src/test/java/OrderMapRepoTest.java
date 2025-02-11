186972933cdfdeef89d4de1bf61311ef0ef3ae92
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-11 12:11:41 +0100

    add class OrderMapRepoTest. copy all tests from OrderListRepoTest and only changed type from OrderListRepo to OrderMapRepo. All tests fit immediately!

---
+++ src/test/java/OrderMapRepoTest.java
@@ -0,0 +1,105 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderMapRepoTest {
+    @Test
+    public void OrderMapRepoExpectedOrderExistsWhenAdded() {
+        // given
+        OrderMapRepo orderMapRepo = new OrderMapRepo();
+        int orderID = 0;
+        Product product = new Product(Long.valueOf("4023222992300"), "Märklin Electric Locomotive",
+                "Märklin", "HO", "Locomotive",
+                "Electric locomotive with sound", BigDecimal.valueOf(349.99), 2022);
+        int quantity = 12;
+        HashMap<Product, Integer> productIntegerHashMap = new HashMap<Product, Integer>();
+        productIntegerHashMap.put(product, quantity);
+        Order order = new Order(orderID, productIntegerHashMap);
+
+        // when
+        orderMapRepo.add(order);
+        Order orderActual = orderMapRepo.getOrder(orderID);
+
+        // then
+        assertEquals(order, orderActual);
+    }
+
+    @Test
+    public void OrderMapRepoExpectedSize1WhenSameOrderAddedTwice() {
+        // given
+        OrderMapRepo orderMapRepo = new OrderMapRepo();
+        Product product = new Product(Long.valueOf("4023222992300"), "Märklin Electric Locomotive",
+                "Märklin", "HO", "Locomotive",
+                "Electric locomotive with sound", BigDecimal.valueOf(349.99), 2022);
+        int quantity = 12;
+        HashMap<Product, Integer> productIntegerHashMap = new HashMap<Product, Integer>();
+        productIntegerHashMap.put(product, quantity);
+        Order order = new Order(0, productIntegerHashMap);
+        int expectedSize = 1;
+
+        // when
+        orderMapRepo.add(order);
+        orderMapRepo.add(order);
+        int actualSize = orderMapRepo.size();
+
+        // then
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void OrderMapRepoExpectedOrderContainedInListByGetAllOrders() {
+        // given
+        OrderMapRepo orderMapRepo = new OrderMapRepo();
+        Product product = new Product(Long.valueOf("4023222992300"), "Märklin Electric Locomotive",
+                "Märklin", "HO", "Locomotive",
+                "Electric locomotive with sound", BigDecimal.valueOf(349.99), 2022);
+        int quantity = 12;
+        HashMap<Product, Integer> productIntegerHashMap = new HashMap<Product, Integer>();
+        productIntegerHashMap.put(product, quantity);
+        Order order = new Order(0, productIntegerHashMap);
+        orderMapRepo.add(order);
+
+        // when
+        List<Order> allOrders = orderMapRepo.getAllOrders();
+
+        // then
+        assertTrue(allOrders.contains(order));
+    }
+
+    @Test
+    public void OrderMapRepoExpectedSize1WhenOrderAdded0WhenOrderRemoved() {
+        // given
+        OrderMapRepo orderMapRepo = new OrderMapRepo();
+        int orderID = 0;
+        Product product = new Product(Long.valueOf("4023222992300"), "Märklin Electric Locomotive",
+                "Märklin", "HO", "Locomotive",
+                "Electric locomotive with sound", BigDecimal.valueOf(349.99), 2022);
+        int quantity = 12;
+        HashMap<Product, Integer> productIntegerHashMap = new HashMap<Product, Integer>();
+        productIntegerHashMap.put(product, quantity);
+        Order order = new Order(orderID, productIntegerHashMap);
+        int expectedSize = 1;
+
+        // when
+        orderMapRepo.add(order);
+        int actualSize = orderMapRepo.size();
+
+        // then
+        assertEquals(expectedSize, actualSize);
+
+        // given
+        expectedSize = 0;
+
+        // when
+        orderMapRepo.removeOrder(orderID);
+        actualSize = orderMapRepo.size();
+
+        // then
+        assertEquals(expectedSize, actualSize);
+    }
+}

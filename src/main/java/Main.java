62f3886a9bf14ea0bc559df164a6b2feb13dd9e0
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-13 11:04:42 +0100

    start implementing change of return getPRoduct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
+
+    //        OrderListRepo orderRepo = new OrderListRepo();
+    static OrderMapRepo orderRepo = new OrderMapRepo();
+    static ShopService shopService = new ShopService(orderRepo);
+
     public static void main(String[] args) {
-//        OrderListRepo orderRepo = new OrderListRepo();
-        OrderMapRepo orderRepo = new OrderMapRepo();
-        ShopService shopService = new ShopService(orderRepo);
         String userInput = "";
 
 //        HashMap<Long, Integer> orderItems = new HashMap<Long, Integer>();
@@ -23,7 +26,7 @@ public class Main {
         {
             System.out.println();
             System.out.println("What do you want to do? You have uncountable possibilities in here!");
-            System.out.println("Input p to see all products, o to place an order, s to see all orders,m to modify an order, r to remove an order.");
+            System.out.println("Input p to see all products, o to place an order, s to see all orders, m to modify an order, r to remove an order.");
             System.out.println("And if you really really want: input q to quit.");
 
             Scanner scanner = new Scanner(System.in);
@@ -32,12 +35,44 @@ public class Main {
             switch (userInput) {
                 case "q":
                     break;
+                case "p":
+                    printAllProducts();
+                    break;
                 default:
-                    continue;
             }
         }
 
         System.out.println();
         System.out.println("Goodbye!");
     }
-}
+
+    static void printAllProducts() {
+        System.out.println(shopService.productRepo.getAllProducts().toString().replaceAll("\\[", "\n["));
+    }
+
+    static void placeOrder() {
+        String productEan = "";
+        int quantity = 0;
+        Scanner userInputScanner = new Scanner(System.in);
+        HashMap<Product, Integer> productIntegerHashMap = new HashMap<Product, Integer>();
+
+        while (!productEan.equals(("f"))) {
+            System.out.println();
+            System.out.println("Please enter the EAN of a product you want to order or f to finish your order");
+            productEan = userInputScanner.nextLine();
+            System.out.println("Please enter the quantity you want to order");
+            quantity = Integer.valueOf(userInputScanner.nextLine());
+
+            if (productEan != "" && quantity > 0) {
+                Optional<Product> product = shopService.productRepo.getProduct(Long.valueOf(productEan));
+
+                if (product.isPresent())
+                    productIntegerHashMap.put(product.get(), quantity);                }
+                else
+                    System.out.println("Product does not exist!");
+            }
+
+        shopService.placeOrder(shopService.orderRepo.size(), productIntegerHashMap);
+        }
+    }
+

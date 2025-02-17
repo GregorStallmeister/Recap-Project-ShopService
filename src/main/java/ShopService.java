881fa9c61dc47c29bb52014f082a7057ee0c6554
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-17 09:40:15 +0100

    solve warning.

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -48,31 +48,21 @@ public class ShopService {
     }
 
     private void fillProductRepo() {
-//        URL url = this.getClass().getResource("EAN-List_csv_headers_modified_shortened.txt");
-//        File file = new File(String.valueOf(url));
-//        File file = new File("EAN-List_csv_headers_modified_shortened.txt");
-//
-//        try {
-//            Scanner fileReader = new Scanner(file);
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Error: " + e.toString());
-//        }
+        String[] eanListEntriesFromChatGPT = new String[12];
+        eanListEntriesFromChatGPT[0] = "4005279537016, \"Br 219 Diesel Locomotive\", \"Fleischmann\", \"HO\", \"Locomotive\", \"A diesel locomotive in red color\", 159.99, 2021";
+        eanListEntriesFromChatGPT[1] = "4015615365285, \"Märklin Tanker Car Set\", \"Märklin\", \"HO\", \"Freight Car\", \"Set of 4 tankers with different designs\", 99.99, 2022";
+        eanListEntriesFromChatGPT[2] = "4053085005343, \"Bachmann Signal Bridge\", \"Bachmann\", \"N\", \"Accessory\", \"Signal bridge for N-scale layout\", 49.99, 2023";
+        eanListEntriesFromChatGPT[3] = "4001883685027, \"Piko Freight Train Set\", \"Piko\", \"HO\", \"Train Set\", \"Complete freight train set with 4 cars\", 229.99, 2020";
+        eanListEntriesFromChatGPT[4] = "4010115512301, \"Roco Digital Controller\", \"Roco\", \"HO\", \"Accessory\", \"Digital controller for HO scale layouts\", 199.99, 2023";
+        eanListEntriesFromChatGPT[5] = "4005265181907, \"Fleischmann Passenger Car\", \"Fleischmann\", \"HO\", \"Passenger Car\", \"Coaches for passenger trains in blue\", 59.99, 2021";
+        eanListEntriesFromChatGPT[6] = "4023222992300, \"Märklin Electric Locomotive\", \"Märklin\", \"HO\", \"Locomotive\", \"Electric locomotive with sound\", 349.99, 2022";
+        eanListEntriesFromChatGPT[7] = "4056231348903, \"Bachmann Train Station\", \"Bachmann\", \"N\", \"Building\", \"A detailed N-scale train station\", 89.99, 2023";
+        eanListEntriesFromChatGPT[8] = "4019655942579, \"Piko Track Set\", \"Piko\", \"HO\", \"Accessory\", \"Track set for building layouts\", 49.99, 2021";
+        eanListEntriesFromChatGPT[9] = "4055086012498, \"Roco Freight Wagon\", \"Roco\", \"HO\", \"Freight Car\", \"A detailed freight wagon in red\", 69.99, 2022";
+        eanListEntriesFromChatGPT[10] = "4005971320239, \"Fleischmann Electric Railcar\", \"Fleischmann\", \"HO\", \"Railcar\", \"Electric railcar in vintage livery\", 119.99, 2020";
+        eanListEntriesFromChatGPT[11] = "4005245310198, \"Märklin Starter Set\", \"Märklin\", \"HO\", \"Train Set\", \"Complete starter set with controller and track\", 399.99, 2023";
 
-        String eanListFromChatGpt = "4005279537016, \"Br 219 Diesel Locomotive\", \"Fleischmann\", \"HO\", \"Locomotive\", \"A diesel locomotive in red color\", 159.99, 2021\n" +
-                "4015615365285, \"Märklin Tanker Car Set\", \"Märklin\", \"HO\", \"Freight Car\", \"Set of 4 tankers with different designs\", 99.99, 2022\n" +
-                "4053085005343, \"Bachmann Signal Bridge\", \"Bachmann\", \"N\", \"Accessory\", \"Signal bridge for N-scale layout\", 49.99, 2023\n" +
-                "4001883685027, \"Piko Freight Train Set\", \"Piko\", \"HO\", \"Train Set\", \"Complete freight train set with 4 cars\", 229.99, 2020\n" +
-                "4010115512301, \"Roco Digital Controller\", \"Roco\", \"HO\", \"Accessory\", \"Digital controller for HO scale layouts\", 199.99, 2023\n" +
-                "4005265181907, \"Fleischmann Passenger Car\", \"Fleischmann\", \"HO\", \"Passenger Car\", \"Coaches for passenger trains in blue\", 59.99, 2021\n" +
-                "4023222992300, \"Märklin Electric Locomotive\", \"Märklin\", \"HO\", \"Locomotive\", \"Electric locomotive with sound\", 349.99, 2022\n" +
-                "4056231348903, \"Bachmann Train Station\", \"Bachmann\", \"N\", \"Building\", \"A detailed N-scale train station\", 89.99, 2023\n" +
-                "4019655942579, \"Piko Track Set\", \"Piko\", \"HO\", \"Accessory\", \"Track set for building layouts\", 49.99, 2021\n" +
-                "4055086012498, \"Roco Freight Wagon\", \"Roco\", \"HO\", \"Freight Car\", \"A detailed freight wagon in red\", 69.99, 2022\n" +
-                "4005971320239, \"Fleischmann Electric Railcar\", \"Fleischmann\", \"HO\", \"Railcar\", \"Electric railcar in vintage livery\", 119.99, 2020\n" +
-                "4005245310198, \"Märklin Starter Set\", \"Märklin\", \"HO\", \"Train Set\", \"Complete starter set with controller and track\", 399.99, 2023";
-        String[] eanListEntries = eanListFromChatGpt.split("\n");
-
-        for (String eanListEntry : eanListEntries) {
+        for (String eanListEntry : eanListEntriesFromChatGPT) {
             productRepo.addProduct(eanListEntry);
         }
     }
